01807a6e37756186c8b48bd74f1bfc4639fa2417
IMRAN AHMED <project.developer.tech@gmail.com>
2026-07-19 09:55:26 +0600

    Add files via upload

---
+++ config/firebase-config.js
@@ -0,0 +1,17 @@
+// ══════════════════════════════════════
+//  FIREBASE CONFIG — keep this file in .gitignore!
+//  Do not upload it to GitHub or any public place.
+// ══════════════════════════════════════
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRrHCBaMr02F-o5VvfMaO1y8KEeaYuRhw",
+  authDomain: "techverse-site.firebaseapp.com",
+  projectId: "techverse-site",
+  storageBucket: "techverse-site.firebasestorage.app",
+  messagingSenderId: "808439176198",
+  appId: "1:808439176198:web:cb9fc3f5841f8cda887787",
+  measurementId: "G-0JNMVMXTC3"
+};
+
+export default firebaseConfig;
